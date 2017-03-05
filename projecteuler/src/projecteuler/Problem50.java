3addc49cfac51b58046f3d9bc1497731f84b32d0
Jacques Botha <Jacques.Botha@4cgroup.co.za>
2017-03-05 15:47:58 +0200

    Added solution to Problem 50.

---
+++ projecteuler/src/projecteuler/Problem50.java
@@ -0,0 +1,69 @@
+package projecteuler;
+
+import java.util.ArrayList;
+
+public class Problem50 {
+
+  public static void main(String[] args) {
+    long startTime = System.nanoTime();
+
+    int upperLimit = 1_000_000;
+    ArrayList<Integer> primes = new ArrayList<>();
+    primes.add(2);
+
+    for (int i = 3; i < upperLimit; i++) {
+      if (isPrime(i)) {
+        primes.add(i);
+      }
+    }
+
+    int maxSum = 0;
+    int p = 0;
+    while (maxSum < upperLimit) {
+      maxSum += primes.get(p);
+      p++;
+    }
+
+    int currentPrime = 0;
+    int currentNumConsecutive = 0;
+    int numConsecutive = p - 1;
+
+    WHILE: while (numConsecutive > 2) {
+      for (int i = 0; i < primes.size() - numConsecutive; i++) {
+        int sum = 0;
+        for (int j = i; j < i + numConsecutive; j++) {
+          sum += primes.get(j);
+        }
+        if (isPrime(sum) && sum < upperLimit) {
+          currentPrime = sum;
+          currentNumConsecutive = numConsecutive;
+          break WHILE;
+        }
+      }
+      numConsecutive--;
+    }
+
+    System.out.println("The prime below " + upperLimit
+        + " that can be written as "
+        + "the sum of the most consecutive primes is " + currentPrime);
+
+    long endTime = System.nanoTime();
+    System.out.println("Execution time: " + (endTime - startTime) / 1000000
+        + "ms");
+  }
+
+  public static boolean isPrime(int num) {
+    if (num <= 1) {
+      return false;
+    }
+    boolean isPrime = true;
+    for (long i = 2l; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    return isPrime;
+  }
+
+}
